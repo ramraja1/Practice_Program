5e119fb217d31a28395b635c46ee931fe3c4a9d5
Pawan <ramtiwari7081@gmail.com>
2024-09-12 15:25:31 +0530

    Adding React Practice Folder

---
+++ React_Practice/Practice/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Title from "./assets/title";
+
+function App() {
+  return (
+    <>
+      <Title t={"Grocery Shop"}></Title>
+    </>
+  );
+}
+
+export default App;
---
+++ React_Practice/Practice/src/assets/title.jsx
@@ -0,0 +1,8 @@
+function Title({t}) {
+  return (
+    <>
+      <h1>{t}</h1>
+    </>
+  );
+}
+export default Title;
